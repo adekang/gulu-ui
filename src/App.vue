1666cedf7f318f7e7f0b7d425866d756d112fac6
TopLong <201497709@qq.com>
2021-04-02 20:45:10 +0800

    路由切换关闭

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,20 @@
 <script lang="ts">
 
 import {ref, provide} from 'vue';
+import {router} from './router';
 
 export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth;
     // 初始值
-    const menuVisible = ref(width <= 500 ? false : true);
+    const menuVisible = ref(width > 500);
     provide('menuVisible', menuVisible);
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false;
+      }
+    });
   }
 };
 </script>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -6,7 +6,7 @@
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/swich">Switch 组件</router-link>
+            <router-link to="/doc/switch">Switch 组件</router-link>
           </li>
           <li>
             <router-link to="/doc/button">Button 组件</router-link>
@@ -19,7 +19,9 @@
           </li>
         </ol>
       </aside>
-      <main>主内容</main>
+      <main>
+        <router-view/>
+      </main>
     </div>
   </div>
 </template>
@@ -40,6 +42,7 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
+
   > h2 {
     margin-bottom: 4px;
   }
