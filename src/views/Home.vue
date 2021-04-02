b2a76471f64894b6ef896d3419e3876d45cecf35
TopLong <201497709@qq.com>
2021-04-02 16:53:30 +0800

    封装TopNav 组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="topNav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <TopNav/>
 
     <div class="banner">
       <h1>UI</h1>
@@ -18,23 +12,15 @@
     </div>
   </div>
 </template>
+<script lang="ts">
+
+import TopNav from '../components/TopNav.vue'
+
+export default {
+  components: {TopNav}
+}
+</script>
 
 <style lang="scss" scoped>
-.topNav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
+
 </style>
