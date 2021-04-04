9918ecb3a78382b0921705852e8de47f8f37e63b
TopLong <201497709@qq.com>
2021-04-04 09:30:23 +0800

    渲染 tabs

--- src/components/Doc/TabsDemo.vue
+++ src/components/Doc/TabsDemo.vue
@@ -3,7 +3,6 @@
 <Tabs>
   <Tab title="导航1">内容1</Tab>
   <Tab title="导航2">内容2</Tab>
-
 </Tabs>
 </template>
 
--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <Slot/>
+    <slot/>
   </div>
 </template>
 
 <script lang='ts'>
-
+export default {};
 </script>
 
 <style lang='scss' scoped>
