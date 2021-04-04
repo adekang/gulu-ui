1310f660f19cb95ba3bd448e9cbd10fb8ec98384
TopLong <201497709@qq.com>
2021-04-04 10:19:00 +0800

    使用CSS切换

--- src/components/Doc/TabsDemo.vue
+++ src/components/Doc/TabsDemo.vue
@@ -1,20 +1,26 @@
 <template>
   <h1>示例</h1>
-<Tabs>
-  <Tab title="导航1">内容1</Tab>
-  <Tab title="导航2">内容2</Tab>
-</Tabs>
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 
 <script lang='ts'>
-import Tab from '../../lib/Tab.vue'
-import Tabs from '../../lib/Tabs.vue'
-export  default  {
-  components:{
+import Tab from '../../lib/Tab.vue';
+import Tabs from '../../lib/Tabs.vue';
+import {ref} from 'vue';
+
+export default {
+  components: {
     Tab,
     Tabs
+  },
+  setup() {
+    const x = ref('导航1');
+    return {x};
   }
-}
+};
 
 </script>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+      <div :class="{selected:t === selected}"
+           @click="select(t)"
+           class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">
+        {{ t }}
+      </div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      <component class="gulu-tabs-content-item"
+                 :class="{selected: c.props.title === selected }"
+                 v-for="c in defaults" :is="c"
+      />
     </div>
   </div>
 </template>
@@ -12,7 +19,13 @@
 <script lang='ts'>
 import Tab from './Tab.vue';
 
+
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -20,12 +33,18 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab');
       }
     });
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
+
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
     return {
       defaults,
-      titles
+      titles,
+      select
+
     };
   }
 };
@@ -58,6 +77,14 @@ $border-color: #d9d9d9;
 
   &-content {
     padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
