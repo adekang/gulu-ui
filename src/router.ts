2dd1120491269fb00bc9975633edca7893b63f18
TopLong <201497709@qq.com>
2021-04-05 15:23:37 +0800

    Markdown.vue 存在新的bug

--- src/router.ts
+++ src/router.ts
@@ -5,13 +5,15 @@ import SwitchDemo from './components/Doc/SwitchDemo.vue';
 import ButtonDemo from './components/Doc/ButtonDemo.vue';
 import DialogDemo from './components/Doc/DialogDemo.vue';
 import TabsDemo from './components/Doc/TabsDemo.vue';
-import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue';
-import Install from './views/Install.vue';
+import {h} from 'vue';
+import Markdown from './components/Markdown.vue';
+
 
 const history = createWebHashHistory();
+const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename});
 export const router = createRouter({
   history: history,
+
   routes: [
     // 默认访问一级页面
     {path: '/', component: Home},
@@ -19,9 +21,9 @@ export const router = createRouter({
       path: '/doc', component: Doc, children: [
         //默认访问的二级页面
         {path: '', component: SwitchDemo},
-        {path: 'intro', component: Intro},
-        {path: 'get-started', component: GetStarted},
-        {path: 'install', component: Install},
+        {path: 'intro', component: md('intro')},
+        {path: 'get-started', component: md('get-started')},
+        {path: 'install', component: md('install')},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
